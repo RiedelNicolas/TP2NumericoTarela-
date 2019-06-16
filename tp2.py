a68a7d3ae4d83b3512fd3e045552ea7583ef7627
Drasungor <aguscambiano@gmail.com>
2019-06-16 19:24:44 -0300

    Se empieza a simular la velocidad y altura de felix

--- tp2.py
+++ tp2.py
@@ -8,11 +8,24 @@ from metodos_numericos import *
 from math import *
 
 def aceleracion(alpha, beta, velocidad, posicion):
+    #argumentos: (alpha, beta, velocidad, posicion, t), t esta para
+    #que siga el formato
     return - ACELERACION_GRAVEDAD+beta*exp(posicion/alpha)*(velocidad**2)
 
-def obtener_velocidad_y_altura():
-    
+def mismo_valor(x):
+    return x
 
+def obtener_velocidad_y_altura_RK4_primer_tramo(k, ):
+
+    altura_actual = ALTURA_INICIAL
+
+    velocidades=[VELOCIDAD_INICIAL]
+    alturas=[ALTURA_INICIAL]
+
+    while (altura>ALTURA_APERTURA_PARACAIDAS):
+        pass
+
+    RK4_con_valor_previo(k, n_inicial, n_final, f, u_inicial, t0)
 
 
 def TP2():
